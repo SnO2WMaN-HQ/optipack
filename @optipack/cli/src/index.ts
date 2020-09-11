54199e6039f70d9a48fdffc01ab9d3addbd6090e
SnO2WMaN <sno2wman.official@gmail.com>
2020-09-11 21:47:26 +0900

    revert(cli): :bug: Prettierのパーサーはjson-stringfyのままで
    This reverts commit 5684b2aa693a2f13e44431ccaf3ec8e13729373e.

--- @optipack/cli/src/index.ts
+++ @optipack/cli/src/index.ts
@@ -41,7 +41,7 @@ export async function getOption(): Promise<{filePath: string; write: boolean}> {
     .then((content) => format(content, config))
     .then((content) =>
       prettier.format(content, {
-        parser: 'json',
+        parser: 'json-stringify',
       }),
     )
     .then((content) =>
