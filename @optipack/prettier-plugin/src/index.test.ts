7caa576c0afc8c4a4493df10ae79591821b9d9da
SnO2WMaN <sno2wman.official@gmail.com>
2020-09-15 01:40:11 +0900

    test(prettier-plugin-optipack): :bug: Jestの設定を更新して失敗を防ぐ

--- @optipack/prettier-plugin/src/index.test.ts
+++ @optipack/prettier-plugin/src/index.test.ts
@@ -1,10 +1,16 @@
 import {readFileSync} from 'fs';
 import {resolve} from 'path';
-import {format} from 'prettier';
+import {format, Options} from 'prettier';
 import {plugin} from '.';
 
+const prettierFormatOption: Options = {
+  parser: 'json-stringify',
+  filepath: 'package.json',
+  plugins: [plugin],
+};
+
 describe('sort', () => {
-  it('sort', () => {
+  it('1', () => {
     const expected: string = readFileSync(
       resolve(__dirname, 'fixtures', '1_expected.json'),
       'utf8',
@@ -13,11 +19,7 @@ describe('sort', () => {
       resolve(__dirname, 'fixtures', '1.json'),
       'utf8',
     );
-    const actual: string = format(code, {
-      parser: 'json-stringify',
-      filepath: 'package.json',
-      plugins: [plugin],
-    });
+    const actual: string = format(code, prettierFormatOption);
 
     expect(actual).toBe(expected);
   });
